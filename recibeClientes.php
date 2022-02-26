8e5029c462c9214a6417a2685e50b7e0cb792693
Paty1223 <99048509+Paty1223@users.noreply.github.com>
2022-02-26 10:39:57 -0600

    Add files via upload

---
+++ recibeClientes.php
@@ -0,0 +1,35 @@
+<?php
+    $codigo = $_POST['codigo'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $dui = $_POST['dui'];
+    $direccion = $_POST['direccion'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['email'];
+?>
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Codigo</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">DUI</th>
+                <th scope="col">Direccion</th>
+                <th scope="col">Telefono</th>
+                <th scope="col">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo $codigo ?></td>
+                <td><?php echo $nombre ?></td>
+                <td><?php echo $apellido ?></td>
+                <td><?php echo $dui ?></td>
+                <td><?php echo $direccion ?></td>
+                <td><?php echo $telefono ?></td>
+                <td><?php echo $email ?></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
